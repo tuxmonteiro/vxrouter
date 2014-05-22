19b642af0a5ef759586ce2688255560d73cc1bc0
Marcelo Monteiro <m.monteiro@corp.globo.com>
2014-05-22 03:46:34 -0300

    Added Constants class

---
+++ src/main/java/lbaas/Constants.java
@@ -0,0 +1,9 @@
+package lbaas;
+
+public class Constants {
+    static public final String QUEUE_ROUTE_ADD = "route.add";
+    static public final String QUEUE_ROUTE_DEL = "route.del";
+
+    static public final String CONF_INSTANCES  = "instances";
+    static public final String CONF_PORT       = "port";
+}
