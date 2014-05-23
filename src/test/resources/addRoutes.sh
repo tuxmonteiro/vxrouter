49567d67f2417054bc10fecd5bbad09caeb738f2
Marcelo Monteiro <m.monteiro@corp.globo.com>
2014-05-23 12:06:03 -0300

    addRoutes.sh updated

--- src/test/resources/addRoutes.sh
+++ src/test/resources/addRoutes.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-#curl -XPOST 'http://127.0.0.1:9090/route' -d '{"routes":[{"name": "lol.localdomain", "endpoints":[{"host":"127.0.0.1", "port": 8081}]}], "version": 28031974}'
-#curl -XDELETE $router -d '{"name": "lol.localdomain", "endpoints":[{"host":"127.0.0.1", "port": 8081}], "version": 28031974}'
-curl -XPOST 'http://127.0.0.1:9090/virtualhost' -d '{"name": "lol.localdomain", "endpoints":[{"host":"127.0.0.1", "port": 8082}], "version": 28031974}'
-curl -XPOST 'http://127.0.0.1:9090/virtualhost' -d '{"name": "lol2.localdomain", "version": 28031975}'
-curl -XPOST 'http://127.0.0.1:9090/real' -d '{"name": "lol.localdomain", "endpoints":[{"host":"127.0.0.1", "port": 8082}], "version": 28031976}'
-curl -XDELETE 'http://127.0.0.1:9090/real/127.0.0.1%3A8082' -d '{"name": "lol.localdomain", "endpoints":[{"host":"127.0.0.1", "port": 8082}], "version": 28031977}'
+#curl -XPOST 'http://127.0.0.1:9090/route' -d '{"routes":[{"name": "lol.localdomain", "endpoints":[{"host":"127.0.0.1", "port": 8081}]}]}' \
+#  && curl -XPOST 'http://127.0.0.1:9090/version' -d '{"version": 28031974}'
+#curl -XDELETE $router -d '{"name": "lol.localdomain", "endpoints":[{"host":"127.0.0.1", "port": 8081}]}' \
+#  && curl -XPOST 'http://127.0.0.1:9090/version' -d '{"version": 28031975}'
+curl -XPOST 'http://127.0.0.1:9090/virtualhost' -d '{"name": "lol.localdomain", "endpoints":[{"host":"127.0.0.1", "port": 8082}]}' \
+  && curl -XPOST 'http://127.0.0.1:9090/version' -d '{"version": 28031976}'
+curl -XPOST 'http://127.0.0.1:9090/virtualhost' -d '{"name": "lol2.localdomain"}' \
+  && curl -XPOST 'http://127.0.0.1:9090/version' -d '{"version": 28031977}'
+curl -XPOST 'http://127.0.0.1:9090/real' -d '{"name": "lol.localdomain", "endpoints":[{"host":"127.0.0.1", "port": 8082}]}' \
+  && curl -XPOST 'http://127.0.0.1:9090/version' -d '{"version": 28031978}'
+curl -XDELETE 'http://127.0.0.1:9090/real/127.0.0.1%3A8082' -d '{"name": "lol.localdomain", "endpoints":[{"host":"127.0.0.1", "port": 8082}]}' \
+  && curl -XPOST 'http://127.0.0.1:9090/version' -d '{"version": 28031979}'
