874ee4359be97d40f8277060a00bee730e894ecb
Marcelo Monteiro <m.monteiro@corp.globo.com>
2014-08-03 19:10:53 -0300

    FakeLogger (for use at tests) added

---
+++ src/test/java/lbaas/unit/util/FakeLogger.java
@@ -0,0 +1,36 @@
+package lbaas.unit.util;
+
+import org.vertx.java.core.logging.Logger;
+import org.vertx.java.core.logging.impl.LogDelegate;
+
+public class FakeLogger extends Logger {
+
+    public FakeLogger(LogDelegate delegate) {
+        super(delegate);
+    }
+
+    @Override
+    public void info(Object message) {
+        System.out.println(String.format("[%s: INFO] %s", this, message));
+    }
+
+    @Override
+    public void warn(Object message) {
+        System.out.println(String.format("[%s: WARN] %s", this, message));
+    }
+
+    @Override
+    public void error(Object message) {
+        System.out.println(String.format("[%s: ERR] %s", this, message));
+    }
+
+    @Override
+    public void debug(Object message) {
+        System.out.println(String.format("[%s: DEBUG] %s", this, message));
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName();
+    }
+}
