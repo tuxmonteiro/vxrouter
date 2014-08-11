3bff0e51887c6f17c67aa10fb9ab280c7cca15f2
Marcelo Monteiro <m.monteiro@corp.globo.com>
2014-08-10 22:24:38 -0300

    HashAlgorithm refactoring. ConsistentHash is now persistent. Timeout and Trasient State management. Changing hashType supported

--- src/main/java/lbaas/loadbalance/impl/HashPolicy.java
+++ src/main/java/lbaas/loadbalance/impl/HashPolicy.java
@@ -4,9 +4,11 @@
  */
 package lbaas.loadbalance.impl;
 
-import static lbaas.util.HashAlgorithm.HashType.MURMUR3_128;
+import static lbaas.Constants.*;
 
 import java.util.Collection;
+
+import org.vertx.java.core.json.JsonObject;
 
 import lbaas.Client;
 import lbaas.RequestData;
@@ -16,13 +18,43 @@ import lbaas.util.HashAlgorithm;
 
 public class HashPolicy implements ILoadBalancePolicy {
 
+    private ConsistentHash<Client> consistentHash = null;
+    private String                 lastHashType   = null;
+    private long                   lastReset      = System.currentTimeMillis();
+
     @Override
     public Client getChoice(final Collection<Client> clients, final RequestData requestData) {
+
         String sourceIp = requestData.getRemoteAddress();
+        JsonObject properties = requestData.getProperties();
+        String hashType = properties.getString(hashAlgorithmFieldName);
+        long timeout = properties.getLong(cacheTimeOutFieldName, 0L);
+        boolean transientState = properties.getBoolean(transientStateFieldName, false);
+
+        long now = System.currentTimeMillis();
         int numberOfReplicas = 1;
-        ConsistentHash<Client> consistentHash = new ConsistentHash<Client>(new HashAlgorithm(MURMUR3_128),
-                                                                           numberOfReplicas,
-                                                                           clients);
+
+        if (lastHashType == null || consistentHash == null) {
+            lastHashType = hashType;
+            transientState = false;
+            lastReset = now;
+            consistentHash = new ConsistentHash<Client>(
+                    new HashAlgorithm(hashType), numberOfReplicas, clients);
+        }
+
+        if (!lastHashType.equals(hashType)) {
+            consistentHash.rebuild(new HashAlgorithm(hashType), numberOfReplicas, clients);
+            lastHashType = hashType;
+            lastReset = now;
+        } else if (transientState) {
+            consistentHash.rebuild(null, null, clients);
+            lastReset = now;
+        } else if ((lastReset + timeout) < now) {
+            consistentHash.resetCache();
+            lastReset = now;
+        }
+
+        properties.putBoolean(transientStateFieldName, false);
         return consistentHash.get(sourceIp);
     }
 
