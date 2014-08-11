db71495347926b6eed6c7021666b38db0bb4e5fa
Marcelo Monteiro <m.monteiro@corp.globo.com>
2014-08-10 22:32:08 -0300

    ConsistentHash refactoring. Cache support added. Rebuild/Nodes Recalculation support

--- src/main/java/lbaas/util/ConsistentHash.java
+++ src/main/java/lbaas/util/ConsistentHash.java
@@ -5,47 +5,77 @@
 package lbaas.util;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
 public class ConsistentHash<T> {
 
- private final HashAlgorithm hashAlgorithm;
- private final int numberOfReplicas;
- private final SortedMap<Integer, T> circle = new TreeMap<Integer, T>();
+    private HashAlgorithm               hashAlgorithm;
+    private int                         numberOfReplicas;
+    private final SortedMap<Integer, T> circle  = new TreeMap<Integer, T>();
+    private final Map<String, Integer>  mapKeys = new HashMap<>();
 
- public ConsistentHash(HashAlgorithm hashAlgorithm, int numberOfReplicas,
-     Collection<T> nodes) {
-   this.hashAlgorithm = hashAlgorithm;
-   this.numberOfReplicas = numberOfReplicas;
+    public ConsistentHash(HashAlgorithm hashAlgorithm, int numberOfReplicas,
+            Collection<T> nodes) {
+        this.hashAlgorithm = hashAlgorithm;
+        this.numberOfReplicas = numberOfReplicas;
 
-   for (T node : nodes) {
-     add(node);
-   }
- }
+        for (T node : nodes) {
+            add(node);
+        }
+    }
 
- public void add(T node) {
-   for (int i = 0; i < numberOfReplicas; i++) {
-     circle.put(hashAlgorithm.hash(node.toString() + i), node);
-   }
- }
+    public void add(T node) {
+        for (int i = 0; i < numberOfReplicas; i++) {
+            circle.put(hashAlgorithm.hash(node.toString() + i), node);
+        }
+    }
 
- public void remove(T node) {
-   for (int i = 0; i < numberOfReplicas; i++) {
-     circle.remove(hashAlgorithm.hash(node.toString() + i));
-   }
- }
+    public void remove(T node) {
+        for (int i = 0; i < numberOfReplicas; i++) {
+            circle.remove(hashAlgorithm.hash(node.toString() + i));
+        }
+    }
 
- public T get(String key) {
-   if (circle.isEmpty()) {
-     return null;
-   }
-   int hash = hashAlgorithm.hash(key);
-   if (!circle.containsKey(hash)) {
-     SortedMap<Integer, T> tailMap = circle.tailMap(hash);
-     hash = tailMap.isEmpty() ? circle.firstKey() : tailMap.firstKey();
-   }
-   return circle.get(hash);
- }
+    public T get(String key) {
+        if (circle.isEmpty()) {
+            return null;
+        }
+
+        int hash;
+        if (!mapKeys.containsKey(key)) {
+            hash = hashAlgorithm.hash(key);
+            mapKeys.put(key, hash);
+        }
+        hash = mapKeys.get(key);
+
+        if (!circle.containsKey(hash)) {
+            SortedMap<Integer, T> tailMap = circle.tailMap(hash);
+            hash = tailMap.isEmpty() ? circle.firstKey() : tailMap.firstKey();
+        }
+        return circle.get(hash);
+    }
+
+    public void resetCache() {
+        mapKeys.clear();
+    }
+
+    public void rebuild(HashAlgorithm hashAlgorithm, Integer numberOfReplicas,
+            Collection<T> nodes) {
+        if (hashAlgorithm!=null) {
+            this.hashAlgorithm = hashAlgorithm;
+        }
+        if (numberOfReplicas!=null) {
+            this.numberOfReplicas = numberOfReplicas;
+        }
+
+        resetCache();
+        circle.clear();
+        for (T node : nodes) {
+            add(node);
+        }
+    }
 
 }
