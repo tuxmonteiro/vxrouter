b9a2dc02d6ca498773a1b403d7ba24d6b91e99c6
Marcelo Monteiro <m.monteiro@corp.globo.com>
2014-08-10 22:18:15 -0300

    Constants refatoring, properties fieldName added and others static strings added

--- src/main/java/lbaas/Constants.java
+++ src/main/java/lbaas/Constants.java
@@ -9,13 +9,22 @@ public class Constants {
     private Constants() {
     }
 
-    public static final String QUEUE_ROUTE_ADD = "route.add";
-    public static final String QUEUE_ROUTE_DEL = "route.del";
-    public static final String QUEUE_ROUTE_VERSION = "route.version";
-    public static final String QUEUE_HEALTHCHECK_OK = "healthcheck.ok";
-    public static final String QUEUE_HEALTHCHECK_FAIL = "healthcheck.fail";
+    public static final String QUEUE_ROUTE_ADD                   = "route.add";
+    public static final String QUEUE_ROUTE_DEL                   = "route.del";
+    public static final String QUEUE_ROUTE_VERSION               = "route.version";
+    public static final String QUEUE_HEALTHCHECK_OK              = "healthcheck.ok";
+    public static final String QUEUE_HEALTHCHECK_FAIL            = "healthcheck.fail";
 
-    public static final String CONF_INSTANCES  = "instances";
-    public static final String CONF_PORT       = "port";
+    public static final String CONF_INSTANCES                    = "instances";
+    public static final String CONF_PORT                         = "port";
+
+    public static final String loadBalancePolicyFieldName        = "loadBalancePolicy";
+    public static final String persistencePolicyFieldName        = "persistencePolicy";
+    public static final String cacheTimeOutFieldName             = "cacheTimeout";
+    public static final String hashAlgorithmFieldName            = "hashAlgorithm";
+    public static final String transientStateFieldName           = "transientState";
+
+    public static final String defaultLoadBalancePolicy          = "DefaultLoadBalancePolicy";
+    public static final String packageOfLoadBalancePolicyClasses = "lbaas.loadbalance.impl";
 
 }
